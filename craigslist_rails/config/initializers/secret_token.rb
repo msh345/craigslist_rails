1148e8577e885f94db26ad308cd78726ba7ca5e3
Apprentice <jvanstry@gmail.com>
2013-10-07 15:10:56 -0500

    first commit

---
+++ craigslist_rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CraigslistRails::Application.config.secret_key_base = '047718a521d49977a9ca11892f001b80d3a29d1ea5a35278ec287e191309a307b7957bb53bb49c3619bd183a284a1f9b8109bc10387de7b1543f28c6674f90d9'
